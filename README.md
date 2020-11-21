4817b67b1b031a06cfa7f5a678989e6eb8159288
Andi Herlan <herlandi.27@gmail.com>
2020-11-21 19:59:47 +0700

    github like calendar style

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Skrip untuk membuat kalender 2021 menggunakan library `calendR`. Data hari libur nasional diperoleh dari situs [liburnasional.com](https://www.liburnasional.com/kalender-lengkap-2021/).
 
-![kalender 2021](https://raw.githubusercontent.com/akherlan/kalender2021/main/image2.png)
+![kalender 2021](https://raw.githubusercontent.com/akherlan/kalender2021/main/image.png)
 
 ## To Do
 
--- calendar2.R
+++ calendar2.R
@@ -1,7 +1,7 @@
 library("rvest")
 library("dplyr")
 
-# harvesting Indonesia's holiday
+# harvesting Indonesia's holiday data
 page_event <- "https://www.liburnasional.com/kalender-lengkap-2021/" %>% 
   read_html()
 
@@ -30,36 +30,38 @@ holiday <- ifelse(format(tanggal, "%w") %in% c(6, 0), "Weekend", NA)
 holiday[event$hari] <- "Libur Nasional" 
 
 # create calendar
-library("gridExtra")
 library("calendR")
+library("gridExtra")
 
-tm <- bind_cols(table[1:3,],table[4:6,],table[7:9,],table[10:12,],table[13:15,]) %>% 
+kalender <- calendR(
+  year = 2021, 
+  start = "M", 
+  special.days = holiday, 
+  special.col = c("#37b24d", "#b2f2bb"), # holidays col
+  days.col = 1,                          # days text col
+  months.col = "white",                  # months text col
+  mbg.col = "#2e3440",                   # months bg col
+  bg.col = "#f4f4f4",                    # bg col
+  lty = 0,
+  title = "KALENDER 2021",
+  orientation = "l"
+)
+
+tblplot <- bind_cols(table[1:3,],table[4:6,],table[7:9,],table[10:12,],table[13:15,]) %>% 
   `names<-`(paste0("x", 1:10))
 
 keterangan <- tableGrob(
-  d = tm, rows = NULL, cols = NULL,
+  d = tblplot, rows = NULL, cols = NULL,
   theme = ttheme_minimal(
     base_size = 8, 
     base_colour = "grey30"
   )
 )
 
-kalender <- calendR(
-  year = 2021, 
-  start = "M", 
-  special.days = holiday, 
-  special.col = c("pink", "gray92"),  # holidays col
-  days.col = 1,                          # days text col
-  months.col = "white",                  # months text col
-  mbg.col = 4,                           # months bg col
-  bg.col = "#f4f4f4",                    # bg col
-  lty = 0,
-  title = "KALENDER 2021",
-  orientation = "l"
-  # legend.pos = "bottom"
-  # pdf = TRUE,
-  # doc_name = "Kalender 2021",
-  # papersize = "A4"
-)
+doc <- grid.arrange(kalender, keterangan, nrow = 2, heights = c(6,1))
 
-grid.arrange(kalender, keterangan, nrow = 2, heights = c(6,1))
+ggplot2::ggsave(
+  file = "Calendar_2021.pdf", plot = doc,
+  width = 11.69, height = 8.27, # A4 landscape
+  units = "in", dpi = 300
+)
